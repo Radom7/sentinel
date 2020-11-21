f068086c0d11d48c9f29260e2d6669e800ec9f32
linliuxing <929593448@qq.com>
2020-11-21 12:41:55 +0800

    返回布尔值的方式定义资源

--- sentinel_quick_start/src/main/java/com/haiyu/sentinel/controller/TestController.java
+++ sentinel_quick_start/src/main/java/com/haiyu/sentinel/controller/TestController.java
@@ -1,11 +1,13 @@
 package com.haiyu.sentinel.controller;
 
 import com.alibaba.csp.sentinel.Entry;
+import com.alibaba.csp.sentinel.SphO;
 import com.alibaba.csp.sentinel.SphU;
 import com.alibaba.csp.sentinel.slots.block.BlockException;
 import com.alibaba.csp.sentinel.slots.block.RuleConstant;
 import com.alibaba.csp.sentinel.slots.block.flow.FlowRule;
 import com.alibaba.csp.sentinel.slots.block.flow.FlowRuleManager;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -20,21 +22,55 @@ import java.util.List;
  * @Version 1.0
  */
 @RestController
+@Slf4j
 public class TestController {
 
+    /**
+     * 抛出异常的方式定义资源
+     * 使用这种方式当资源发生限流后会抛出BlockException。这个时候可以捕获异常，进行限流之后的逻辑处理
+     * @return
+     */
     @GetMapping("hello")
     public String hello(){
         //使用限流规则
         try(Entry entry = SphU.entry("Hello")) {//限流入口
             return "Hello Sentinel!";//被保护的资源
         } catch (BlockException e) {
+            //被限流或者降级的操作处理
             e.printStackTrace();
-            return "系统繁忙，请稍后";//被限流或者降级的操作处理
+            return "系统繁忙，请稍后";
         }
     }
 
+    /**
+     * 返回布尔值的方式定义资源
+     * 使用的API为SphO，限流后返回的值为boolean类型
+     *
+     * 注意：SphO.entry必须和SphO.exit成对出现 否则会报错
+     */
+    @GetMapping("/boolean")
+    public boolean returnBoolean(){
+        //使用限流规则
+        if (SphO.entry("Sentinel-boolean")){
+            //被保护的资源
+            try {
+                log.info("Hello Sentinel");
+                return true;
+            }finally {
+                SphO.exit();//限流的出口
+            }
+        }else {
+            //被限流或者降级的操作处理
+            log.error("系统繁忙,请稍后再试");
+            return false;
+        }
+    }
+
+
     /** 定义限流规则
      * @PostConstruct 此注解的含义是：本类构造方法执行结束后执行
+     *
+     * 注释掉的原因：不使用硬编码的形式定义限流规则，在sentinel控制台上定义规则进行限流
      */
 //    @PostConstruct
 //    public void initFloRule(){
